e23409ea848c51379ed5aa024999f221adba9e5f
Alif102 <alifahmed102@gmail.com>
2023-11-06 17:05:00 +0600

    Private Route create

--- src/Components/Rooms/Rooms.jsx
+++ src/Components/Rooms/Rooms.jsx
@@ -1,7 +1,35 @@
+import { useEffect, useState } from "react"
+import axios from 'axios';
 
 const Rooms = () => {
+  const [rooms,setRooms] = useState([])
+
+  useEffect( ()=>{
+    try {
+      const data = ( axios.get('/getallrooms')).data
+      setRooms(data)
+      
+    } catch (error) {
+      console.log(error)
+      
+    }
+
+  },[])
+
+  // useEffect(async () => {
+  //   try {
+  //     const data = await axios.get('/getallrooms')
+      
+  //   } catch (error) {
+      
+  //   }
+
+  // },[])
   return (
-    <div>Rooms</div>
+    <div>
+      <h1>there are {rooms.length}</h1>
+
+    </div>
   )
 }
 
