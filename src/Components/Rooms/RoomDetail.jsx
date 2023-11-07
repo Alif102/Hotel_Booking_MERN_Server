fa89b5e422845b077b149dcd38f67ab23568f4a7
Alif102 <alifahmed102@gmail.com>
2023-11-07 19:20:04 +0600

    display single rooms card

--- src/Components/Rooms/RoomDetail.jsx
+++ src/Components/Rooms/RoomDetail.jsx
@@ -1,9 +1,27 @@
 /* eslint-disable react/prop-types */
 
 const RoomDetail = ({details}) => {
-    const {name} = details 
+    const {name , image, rentperday} = details 
   return (
-    <div>RoomDetail {name}</div>
+    <div>
+         <div   className="card w-[270px] bg-base-100 shadow-xl">
+           
+           
+
+           <figure><img src={image} alt="img" /></figure>
+ <div className="card-body">
+   <p> <span className='font-bold text-cyan-400'>${rentperday}</span> / Per Night</p>
+   <h2 className="card-title text-cyan-400">{name} </h2>
+   
+   <div className="card-actions"> 
+   <button className="btn btn-accent">View Details</button>
+
+ 
+   </div>
+ </div>
+
+         </div>
+         </div>
   )
 }
 
